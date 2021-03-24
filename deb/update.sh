1ae1c1814d99a43fc8b5f063f6ddd6b3d8544c3b
Matt Evans <matthewevans@users.noreply.github.com>
2021-03-24 04:17:19 -0400

    Refactor repo. Use freight to generate apt repo

--- deb/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find . -maxdepth 1 -type d -name '[a-z]*' -exec bash -c "cd {} && dpkg-scanpackages --multiversion . > Packages && gzip -k -f Packages && apt-ftparchive release . > Release" \;
-
